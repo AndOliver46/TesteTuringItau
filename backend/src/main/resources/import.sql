aa5068c9bd9a5cd865345d2312b1a2fc06de3ca4
AndOliver46 <anderson_souza75@live.com>
2023-02-27 20:38:13 -0300

    Changing balance of users

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -9,8 +9,8 @@ INSERT INTO tb_cliente(cpf, nome) VALUES ('86987675899', 'Ana Green');
 INSERT INTO tb_cliente(cpf, nome) VALUES ('65674654633', 'Bob Brown');
 INSERT INTO tb_cliente(cpf, nome) VALUES ('76574876577', 'Alex Blue');
 
-INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21554', 0.0, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 1, 1);
-INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21675', 0.0, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 1, 2);
-INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21564', 15000.0, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 3, 3);
-INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21567', 0.0, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 2, 4);
-INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('26156', 0.0, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 4, 5);
+INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21554', 2000.54, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 1, 1);
+INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21675', 500000.54, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 1, 2);
+INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21564', 30000.76, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 3, 3);
+INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('21567', 43987.98, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 2, 4);
+INSERT INTO tb_conta(numero, saldo, senha, authoritie, agencia_id, id_cliente) VALUES ('26156', 56746.00, '$2a$10$acoc/IcsjzXAISDJmIN9w.ywkuKg6wAGZGP3m.rzIu9eymK6rpiKq', 'ROLE_CLIENT', 4, 5);
